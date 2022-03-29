78e26801210fcf91f6493837364d676ec7307b77
suwinyaa <suwinya.2543@gmail.com>
2022-03-29 20:43:04 +0700

    edit main

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
 @app.get("/test")
-def read_root():
+def read_test():
     return {"Hello": "test"}
 
 handler = Mangum(app)
